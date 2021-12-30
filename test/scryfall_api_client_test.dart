b90ee81a631c6d7ed022b21c263b9e2c444107ed
Aron Ingruber <aron@ingruber.net>
2021-12-30 11:17:47 +0100

    [#7] Add method to search for cards

--- test/scryfall_api_client_test.dart
+++ test/scryfall_api_client_test.dart
@@ -496,5 +496,263 @@ void main() {
         expect(actual, isA<MtgSet>());
       });
     });
+
+    group('getCardsBySearch', () {
+      final searchQuery = 'c:red pow=3';
+      final sortingOrder = SortingOrder.cmc;
+
+      test('makes correct http request', () async {
+        final response = MockResponse();
+        when(() => response.statusCode).thenReturn(200);
+        when(() => response.body).thenReturn('{}');
+        when(() => httpClient.get(any())).thenAnswer((_) async => response);
+        try {
+          await scryfallApiClient.searchCards(
+            searchQuery,
+            sortingOrder: sortingOrder,
+          );
+        } catch (_) {}
+        verify(
+          () => httpClient.get(Uri.https(
+            'api.scryfall.com',
+            '/cards/search',
+            {
+              'q': searchQuery,
+              'order': sortingOrder.name,
+            },
+          )),
+        ).called(1);
+      });
+
+      test('throws ScryfallException on non-200 response', () async {
+        final json = jsonEncode({
+          'object': 'error',
+          'code': 'not_found',
+          'status': 404,
+          'details': 'The requested object or REST method was not found.',
+        });
+
+        final response = MockResponse();
+        when(() => response.statusCode).thenReturn(404);
+        when(() => response.body).thenReturn(json);
+        when(() => httpClient.get(any())).thenAnswer((_) async => response);
+        await expectLater(
+          scryfallApiClient.searchCards(
+            searchQuery,
+            sortingOrder: sortingOrder,
+          ),
+          throwsA(isA<ScryfallException>()),
+        );
+      });
+
+      test('returns PaginableList<MtgCard> on valid response', () async {
+        final totalCards = 566;
+        final hasMore = true;
+        final nextPageStr =
+            'https://api.scryfall.com/cards/search?format=json&include_extras=false&include_multilingual=false&order=cmc&page=2&q=c%3Ared+pow%3D3&unique=cards';
+        final data = [
+          {
+            'object': 'card',
+            'id': 'd99a9a7d-d9ca-4c11-80ab-e39d5943a315',
+            'oracle_id': '1f74e26f-5cf3-4b0e-bcc6-f858599474fc',
+            'multiverse_ids': [522262],
+            'mtgo_id': 90749,
+            'tcgplayer_id': 239722,
+            'cardmarket_id': 566268,
+            'name': 'Asmoranomardicadaistinaculdacar',
+            'lang': 'en',
+            'released_at': '2021-06-18',
+            'uri':
+                'https://api.scryfall.com/cards/d99a9a7d-d9ca-4c11-80ab-e39d5943a315',
+            'scryfall_uri':
+                'https://scryfall.com/card/mh2/186/asmoranomardicadaistinaculdacar?utm_source=api',
+            'layout': 'normal',
+            'highres_image': true,
+            'image_status': 'highres_scan',
+            'image_uris': {
+              'small':
+                  'https://c1.scryfall.com/file/scryfall-cards/small/front/d/9/d99a9a7d-d9ca-4c11-80ab-e39d5943a315.jpg?1632831210',
+              'normal':
+                  'https://c1.scryfall.com/file/scryfall-cards/normal/front/d/9/d99a9a7d-d9ca-4c11-80ab-e39d5943a315.jpg?1632831210',
+              'large':
+                  'https://c1.scryfall.com/file/scryfall-cards/large/front/d/9/d99a9a7d-d9ca-4c11-80ab-e39d5943a315.jpg?1632831210',
+              'png':
+                  'https://c1.scryfall.com/file/scryfall-cards/png/front/d/9/d99a9a7d-d9ca-4c11-80ab-e39d5943a315.png?1632831210',
+              'art_crop':
+                  'https://c1.scryfall.com/file/scryfall-cards/art_crop/front/d/9/d99a9a7d-d9ca-4c11-80ab-e39d5943a315.jpg?1632831210',
+              'border_crop':
+                  'https://c1.scryfall.com/file/scryfall-cards/border_crop/front/d/9/d99a9a7d-d9ca-4c11-80ab-e39d5943a315.jpg?1632831210'
+            },
+            'mana_cost': '',
+            'cmc': 0,
+            'type_line': 'Legendary Creature — Human Wizard',
+            'oracle_text':
+                'As long as you\'ve discarded a card this turn, you may pay {B/R} to cast this spell.\nWhen Asmoranomardicadaistinaculdacar enters the battlefield, you may search your library for a card named The Underworld Cookbook, reveal it, put it into your hand, then shuffle.\nSacrifice two Foods: Target creature deals 6 damage to itself.',
+            'power': '3',
+            'toughness': '3',
+            'colors': ['B', 'R'],
+            'color_indicator': ['B', 'R'],
+            'color_identity': ['B', 'R'],
+            'keywords': [],
+            'all_parts': [
+              {
+                'object': 'related_card',
+                'id': 'd99a9a7d-d9ca-4c11-80ab-e39d5943a315',
+                'component': 'combo_piece',
+                'name': 'Asmoranomardicadaistinaculdacar',
+                'type_line': 'Legendary Creature — Human Wizard',
+                'uri':
+                    'https://api.scryfall.com/cards/d99a9a7d-d9ca-4c11-80ab-e39d5943a315'
+              },
+              {
+                'object': 'related_card',
+                'id': '039d62b0-3309-4424-a2ea-5a0d88d4bd72',
+                'component': 'combo_piece',
+                'name': 'The Underworld Cookbook',
+                'type_line': 'Artifact',
+                'uri':
+                    'https://api.scryfall.com/cards/039d62b0-3309-4424-a2ea-5a0d88d4bd72'
+              }
+            ],
+            'legalities': {
+              'standard': 'not_legal',
+              'future': 'not_legal',
+              'historic': 'not_legal',
+              'gladiator': 'not_legal',
+              'pioneer': 'not_legal',
+              'modern': 'legal',
+              'legacy': 'legal',
+              'pauper': 'not_legal',
+              'vintage': 'legal',
+              'penny': 'not_legal',
+              'commander': 'legal',
+              'brawl': 'not_legal',
+              'historicbrawl': 'not_legal',
+              'alchemy': 'not_legal',
+              'paupercommander': 'not_legal',
+              'duel': 'restricted',
+              'oldschool': 'not_legal',
+              'premodern': 'not_legal'
+            },
+            'games': ['paper', 'mtgo'],
+            'reserved': false,
+            'foil': true,
+            'nonfoil': true,
+            'finishes': ['nonfoil', 'foil'],
+            'oversized': false,
+            'promo': false,
+            'reprint': false,
+            'variation': false,
+            'set_id': 'c1c7eb8c-f205-40ab-a609-767cb296544e',
+            'set': 'mh2',
+            'set_name': 'Modern Horizons 2',
+            'set_type': 'draft_innovation',
+            'set_uri':
+                'https://api.scryfall.com/sets/c1c7eb8c-f205-40ab-a609-767cb296544e',
+            'set_search_uri':
+                'https://api.scryfall.com/cards/search?order=set&q=e%3Amh2&unique=prints',
+            'scryfall_set_uri': 'https://scryfall.com/sets/mh2?utm_source=api',
+            'rulings_uri':
+                'https://api.scryfall.com/cards/d99a9a7d-d9ca-4c11-80ab-e39d5943a315/rulings',
+            'prints_search_uri':
+                'https://api.scryfall.com/cards/search?order=released&q=oracleid%3A1f74e26f-5cf3-4b0e-bcc6-f858599474fc&unique=prints',
+            'collector_number': '186',
+            'digital': false,
+            'rarity': 'rare',
+            'card_back_id': '0aeebaf5-8c7d-4636-9e82-8c27447861f7',
+            'artist': 'Ryan Pancoast',
+            'artist_ids': ['89cc9475-dda2-4d13-bf88-54b92867a25c'],
+            'illustration_id': '109aa8fc-4587-4092-9a51-da3a81099ae5',
+            'border_color': 'black',
+            'frame': '2015',
+            'frame_effects': ['legendary'],
+            'security_stamp': 'oval',
+            'full_art': false,
+            'textless': false,
+            'booster': true,
+            'story_spotlight': false,
+            'edhrec_rank': 8208,
+            'preview': {
+              'source': 'Wizards of the Coast',
+              'source_uri':
+                  'https://magic.wizards.com/en/articles/archive/card-preview/challenge-accepted-2021-05-26',
+              'previewed_at': '2021-05-26'
+            },
+            'prices': {
+              'usd': '0.35',
+              'usd_foil': '0.80',
+              'usd_etched': null,
+              'eur': '0.57',
+              'eur_foil': '0.92',
+              'tix': '0.59'
+            },
+            'related_uris': {
+              'gatherer':
+                  'https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=522262',
+              'tcgplayer_infinite_articles':
+                  'https://infinite.tcgplayer.com/search?contentMode=article&game=magic&partner=scryfall&q=Asmoranomardicadaistinaculdacar&utm_campaign=affiliate&utm_medium=api&utm_source=scryfall',
+              'tcgplayer_infinite_decks':
+                  'https://infinite.tcgplayer.com/search?contentMode=deck&game=magic&partner=scryfall&q=Asmoranomardicadaistinaculdacar&utm_campaign=affiliate&utm_medium=api&utm_source=scryfall',
+              'edhrec':
+                  'https://edhrec.com/route/?cc=Asmoranomardicadaistinaculdacar',
+              'mtgtop8':
+                  'https://mtgtop8.com/search?MD_check=1&SB_check=1&cards=Asmoranomardicadaistinaculdacar'
+            },
+            'purchase_uris': {
+              'tcgplayer':
+                  'https://shop.tcgplayer.com/product/productsearch?id=239722&utm_campaign=affiliate&utm_medium=api&utm_source=scryfall',
+              'cardmarket':
+                  'https://www.cardmarket.com/en/Magic/Products/Search?referrer=scryfall&searchString=Asmoranomardicadaistinaculdacar&utm_campaign=card_prices&utm_medium=text&utm_source=scryfall',
+              'cardhoarder':
+                  'https://www.cardhoarder.com/cards/90749?affiliate_id=scryfall&ref=card-profile&utm_campaign=affiliate&utm_medium=card&utm_source=scryfall'
+            }
+          },
+        ];
+
+        final json = jsonEncode({
+          'object': 'list',
+          'total_cards': totalCards,
+          'has_more': hasMore,
+          'next_page': nextPageStr,
+          'data': data,
+        });
+
+        final nextPage = Uri.parse(nextPageStr);
+
+        final response = MockResponse();
+        when(() => response.statusCode).thenReturn(200);
+        when(() => response.body).thenReturn(json);
+        when(() => httpClient.get(any())).thenAnswer((_) async => response);
+        final actual = await scryfallApiClient.searchCards(
+          searchQuery,
+          sortingOrder: sortingOrder,
+        );
+        expect(
+          actual,
+          isA<PaginableList>()
+              .having((l) => l.hasMore, 'hasMore', hasMore)
+              .having((l) => l.nextPage, 'nextPage', nextPage)
+              .having((l) => l.length, 'length', data.length)
+              .having(
+                (l) => l.data,
+                'data',
+                isA<List<MtgCard>>().having(
+                  (l) => l.first,
+                  'first',
+                  isA<MtgCard>(),
+                ),
+              ),
+        );
+      });
+
+      test('gets valid response from actual server', () async {
+        final scryfallApiClientReal = ScryfallApiClient();
+        final actual = await scryfallApiClientReal.searchCards(
+          searchQuery,
+          sortingOrder: sortingOrder,
+        );
+        expect(actual, isA<PaginableList<MtgCard>>());
+      });
+    });
   });
 }
