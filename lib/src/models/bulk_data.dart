3439ba678b7c3d55b475ad8bad783bc296a871a2
Aron Ingruber <aron@ingruber.net>
2022-01-03 11:27:00 +0100

    [#45] Add BulkData model

---
+++ lib/src/models/bulk_data.dart
@@ -0,0 +1,57 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'bulk_data.g.dart';
+
+/// [BulkData] objects represent daily exports of Scryfall's
+/// card data in bulk files.
+@JsonSerializable()
+class BulkData {
+  /// A unique ID for this bulk item.
+  final String id;
+
+  /// The Scryfall API URI for this file.
+  final Uri uri;
+
+  /// A computer-readable string for the kind of bulk item.
+  final String type;
+
+  /// A human-readable name for this file.
+  final String name;
+
+  /// A human-readable description for this file.
+  final String description;
+
+  /// The URI that hosts this bulk file for fetching.
+  final Uri downloadUri;
+
+  /// The time when this file was last updated.
+  final DateTime updatedAt;
+
+  /// The size of this file in integer bytes.
+  final int compressedSize;
+
+  /// The MIME type of this file.
+  final String contentType;
+
+  /// The Content-Encoding encoding that will be used to
+  /// transmit this file when you download it.
+  final String contentEncoding;
+
+  /// Constructs a [BulkData] object by setting its properties.
+  const BulkData({
+    required this.id,
+    required this.uri,
+    required this.type,
+    required this.name,
+    required this.description,
+    required this.downloadUri,
+    required this.updatedAt,
+    required this.compressedSize,
+    required this.contentType,
+    required this.contentEncoding,
+  });
+
+  /// Constructs a [BulkData] object from JSON.
+  factory BulkData.fromJson(Map<String, dynamic> json) =>
+      _$BulkDataFromJson(json);
+}
---
+++ lib/src/models/bulk_data.g.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bulk_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BulkData _$BulkDataFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'BulkData',
+      json,
+      ($checkedConvert) {
+        final val = BulkData(
+          id: $checkedConvert('id', (v) => v as String),
+          uri: $checkedConvert('uri', (v) => Uri.parse(v as String)),
+          type: $checkedConvert('type', (v) => v as String),
+          name: $checkedConvert('name', (v) => v as String),
+          description: $checkedConvert('description', (v) => v as String),
+          downloadUri:
+              $checkedConvert('download_uri', (v) => Uri.parse(v as String)),
+          updatedAt:
+              $checkedConvert('updated_at', (v) => DateTime.parse(v as String)),
+          compressedSize: $checkedConvert('compressed_size', (v) => v as int),
+          contentType: $checkedConvert('content_type', (v) => v as String),
+          contentEncoding:
+              $checkedConvert('content_encoding', (v) => v as String),
+        );
+        return val;
+      },
+      fieldKeyMap: const {
+        'downloadUri': 'download_uri',
+        'updatedAt': 'updated_at',
+        'compressedSize': 'compressed_size',
+        'contentType': 'content_type',
+        'contentEncoding': 'content_encoding'
+      },
+    );
