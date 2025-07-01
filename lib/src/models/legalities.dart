40af478c6af0835ea97f38a4637679351d95e62f
Aron Ingruber <aron@ingruber.net>
2025-07-01 11:24:20 +0200

    Add default values to legalities

--- lib/src/models/legalities.dart
+++ lib/src/models/legalities.dart
@@ -8,91 +8,91 @@ part 'legalities.g.dart';
 @JsonSerializable()
 class Legalities {
   /// Standard playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality standard;
 
   /// Future playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality future;
 
   /// Historic playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality historic;
 
   /// Timeless playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality timeless;
 
   /// Gladiator playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality gladiator;
 
   /// Pioneer playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality pioneer;
 
   /// Explorer playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality explorer;
 
   /// Modern playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality modern;
 
   /// Legacy playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality legacy;
 
   /// Pauper playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality pauper;
 
   /// Vintage playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality vintage;
 
   /// Penny playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality penny;
 
   /// Commander playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality commander;
 
   /// Oathbreaker playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality oathbreaker;
 
   /// Brawl playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality brawl;
 
   /// Standard brawl playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality standardbrawl;
 
   /// Alchemy playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality alchemy;
 
   /// Pauper commander playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality paupercommander;
 
   /// Duel playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality duel;
 
   /// Old-school playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality oldschool;
 
   /// Pre-modern playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality premodern;
 
   /// PreDH playing format.
-  @JsonKey(unknownEnumValue: Legality.unknown)
+  @JsonKey(defaultValue: Legality.unknown, unknownEnumValue: Legality.unknown)
   final Legality predh;
 
   /// Constructs a [Legalities] object by setting its properties.
--- lib/src/models/legalities.g.dart
+++ lib/src/models/legalities.g.dart
@@ -13,92 +13,136 @@ Legalities _$LegalitiesFromJson(Map<String, dynamic> json) => $checkedCreate(
         final val = Legalities(
           standard: $checkedConvert(
               'standard',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           future: $checkedConvert(
               'future',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           historic: $checkedConvert(
               'historic',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           timeless: $checkedConvert(
               'timeless',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           gladiator: $checkedConvert(
               'gladiator',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           pioneer: $checkedConvert(
               'pioneer',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           explorer: $checkedConvert(
               'explorer',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           modern: $checkedConvert(
               'modern',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           legacy: $checkedConvert(
               'legacy',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           pauper: $checkedConvert(
               'pauper',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           vintage: $checkedConvert(
               'vintage',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           penny: $checkedConvert(
               'penny',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           commander: $checkedConvert(
               'commander',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           oathbreaker: $checkedConvert(
               'oathbreaker',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           brawl: $checkedConvert(
               'brawl',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           standardbrawl: $checkedConvert(
               'standardbrawl',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           alchemy: $checkedConvert(
               'alchemy',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           paupercommander: $checkedConvert(
               'paupercommander',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           duel: $checkedConvert(
               'duel',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           oldschool: $checkedConvert(
               'oldschool',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           premodern: $checkedConvert(
               'premodern',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
           predh: $checkedConvert(
               'predh',
-              (v) => $enumDecode(_$LegalityEnumMap, v,
-                  unknownValue: Legality.unknown)),
+              (v) =>
+                  $enumDecodeNullable(_$LegalityEnumMap, v,
+                      unknownValue: Legality.unknown) ??
+                  Legality.unknown),
         );
         return val;
       },
